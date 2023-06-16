249ec6d8572664941ce753953d7c9869b6522a76
BonanzaInterective <devgitbonanza@gmail.com>
2023-06-16 13:14:55 +0530

    Latest commit 4

---
+++ index.ts
@@ -0,0 +1,8 @@
+//import { SlotocrashServer } from "./games/slotocrash/slotocrash_server";
+import { SlotocrashServer } from "./src/games/slotocrash/slotocrash_server";
+import { RGS } from "./src/libs/server/rgs";
+
+const port: string = "8080";
+
+const server = new RGS(new SlotocrashServer());
+server.start(port);
